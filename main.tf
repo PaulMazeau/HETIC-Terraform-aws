83a39d19cc94e6065c607912532594b14e6d51df
Paul mazeau <61906368+PaulMazeau@users.noreply.github.com>
2024-03-14 16:03:13 +0100

    Terraform par Nicolas

---
+++ main.tf
@@ -0,0 +1,319 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_emr_cluster" "cluster" {
+  name          = "emr-test-arn"
+  release_label = "emr-7.0.0"
+  applications  = ["Spark"]
+
+  ec2_attributes {
+    subnet_id                         = aws_subnet.main.id
+    emr_managed_master_security_group = aws_security_group.allow_access.id
+    emr_managed_slave_security_group  = aws_security_group.allow_access.id
+    instance_profile                  = aws_iam_instance_profile.emr_profile.arn
+  }
+
+  master_instance_group {
+    instance_type = "m5.xlarge"
+  }
+
+  core_instance_group {
+    instance_count = 1
+    instance_type  = "m5.xlarge"
+  }
+
+  service_role = aws_iam_role.iam_emr_service_role.arn
+}
+
+resource "aws_security_group" "allow_access" {
+  name        = "allow_access"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  depends_on = [aws_subnet.main]
+
+  lifecycle {
+    ignore_changes = [
+      ingress,
+      egress,
+    ]
+  }
+
+  tags = {
+    name = "emr_test"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "168.31.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    name = "emr_test"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "168.31.0.0/20"
+
+  tags = {
+    name = "emr_test"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_main_route_table_association" "a" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.r.id
+}
+
+###
+
+# IAM Role setups
+
+###
+
+# IAM role for EMR Service
+data "aws_iam_policy_document" "emr_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["elasticmapreduce.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_emr_service_role" {
+  name               = "iam_emr_service_role"
+  assume_role_policy = data.aws_iam_policy_document.emr_assume_role.json
+}
+
+data "aws_iam_policy_document" "iam_emr_service_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:AuthorizeSecurityGroupEgress",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:CancelSpotInstanceRequests",
+      "ec2:CreateNetworkInterface",
+      "ec2:CreateSecurityGroup",
+      "ec2:CreateTags",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DeleteSecurityGroup",
+      "ec2:DeleteTags",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeAccountAttributes",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeInstanceStatus",
+      "ec2:DescribeInstances",
+      "ec2:DescribeKeyPairs",
+      "ec2:DescribeNetworkAcls",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribePrefixLists",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSpotInstanceRequests",
+      "ec2:DescribeSpotPriceHistory",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeVpcAttribute",
+      "ec2:DescribeVpcEndpoints",
+      "ec2:DescribeVpcEndpointServices",
+      "ec2:DescribeVpcs",
+      "ec2:DetachNetworkInterface",
+      "ec2:ModifyImageAttribute",
+      "ec2:ModifyInstanceAttribute",
+      "ec2:RequestSpotInstances",
+      "ec2:RevokeSecurityGroupEgress",
+      "ec2:RunInstances",
+      "ec2:TerminateInstances",
+      "ec2:DeleteVolume",
+      "ec2:DescribeVolumeStatus",
+      "ec2:DescribeVolumes",
+      "ec2:DetachVolume",
+      "iam:GetRole",
+      "iam:GetRolePolicy",
+      "iam:ListInstanceProfiles",
+      "iam:ListRolePolicies",
+      "iam:PassRole",
+      "s3:CreateBucket",
+      "s3:Get*",
+      "s3:List*",
+      "sdb:BatchPutAttributes",
+      "sdb:Select",
+      "sqs:CreateQueue",
+      "sqs:Delete*",
+      "sqs:GetQueue*",
+      "sqs:PurgeQueue",
+      "sqs:ReceiveMessage",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "iam_emr_service_policy" {
+  name   = "iam_emr_service_policy"
+  role   = aws_iam_role.iam_emr_service_role.id
+  policy = data.aws_iam_policy_document.iam_emr_service_policy.json
+}
+
+# IAM Role for EC2 Instance Profile
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_emr_profile_role" {
+  name               = "iam_emr_profile_role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_instance_profile" "emr_profile" {
+  name = "emr_profile"
+  role = aws_iam_role.iam_emr_profile_role.name
+}
+
+data "aws_iam_policy_document" "iam_emr_profile_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "cloudwatch:*",
+      "dynamodb:*",
+      "ec2:Describe*",
+      "elasticmapreduce:Describe*",
+      "elasticmapreduce:ListBootstrapActions",
+      "elasticmapreduce:ListClusters",
+      "elasticmapreduce:ListInstanceGroups",
+      "elasticmapreduce:ListInstances",
+      "elasticmapreduce:ListSteps",
+      "kinesis:CreateStream",
+      "kinesis:DeleteStream",
+      "kinesis:DescribeStream",
+      "kinesis:GetRecords",
+      "kinesis:GetShardIterator",
+      "kinesis:MergeShards",
+      "kinesis:PutRecord",
+      "kinesis:SplitShard",
+      "rds:Describe*",
+      "s3:*",
+      "sdb:*",
+      "sns:*",
+      "sqs:*",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "iam_emr_profile_policy" {
+  name   = "iam_emr_profile_policy"
+  role   = aws_iam_role.iam_emr_profile_role.id
+  policy = data.aws_iam_policy_document.iam_emr_profile_policy.json
+}
+
+# create rds with mongodb
+
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "my-docdb-cluster"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+  vpc_security_group_ids  = [aws_security_group.allow_access_db.id]
+  db_subnet_group_name    = aws_db_subnet_group.subnet_group_db.name
+}
+
+resource "aws_db_subnet_group" "subnet_group_db" {
+  name       = "aws_db_subnet_group"
+  subnet_ids = [aws_subnet.main1_db.id, aws_subnet.main2_db.id, aws_subnet.main3_db.id]
+}
+
+resource "aws_security_group" "allow_access_db" {
+  name        = "allow_access"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.main_db.id
+}
+
+resource "aws_vpc" "main_db" {
+  cidr_block           = "168.31.0.0/16"
+  enable_dns_hostnames = true
+}
+
+
+resource "aws_subnet" "main1_db" {
+  vpc_id     = aws_vpc.main_db.id
+  cidr_block = "168.31.0.0/20"
+  availability_zone = "eu-west-3a"
+}
+
+resource "aws_subnet" "main2_db" {
+  vpc_id     = aws_vpc.main_db.id
+  cidr_block = "168.31.16.0/20"
+  availability_zone = "eu-west-3b"
+}
+
+resource "aws_subnet" "main3_db" {
+  vpc_id     = aws_vpc.main_db.id
+  cidr_block = "168.31.32.0/20"
+  availability_zone = "eu-west-3c"
+}
+
+resource "aws_internet_gateway" "gw_db" {
+  vpc_id = aws_vpc.main_db.id
+}
+
